120638292a746e85958fa0d318bb1d907b4d3a8c
Sungmin Woo <woos8899@gmail.com>
2026-06-01 18:17:25 +0900

    Add miyokuku static lib with JSON wrapper
    Add CMakeLists to build a miyokuku static library and fetch nlohmann/json v3.12.0 via FetchContent. Introduce include/miyokuku/json.hpp which aliases nlohmann::json to miyokuku::Json, and add a minimal src/json.cpp that includes the header. Configure public include directories and link against nlohmann_json::nlohmann_json.

---
+++ include/miyokuku/json.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <nlohmann/json_fwd.hpp>
+
+
+namespace miyokuku {
+
+    using Json = nlohmann::json;
+
+}  // namespace miyokuku
---
+++ src/json.cpp
@@ -0,0 +1,1 @@
+#include "miyokuku/json.hpp"
